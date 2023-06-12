9e2606249f40ca412d2d2e354980e51f39f8aea5
je223xafhw <stud105859@fh-wedel.de>
2023-06-12 17:09:17 +0200

    added toLower

--- code/04/07_galgen.hs
+++ code/04/07_galgen.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 hangman :: String -> IO ()
 hangman s = hangman' s "" 0
 
@@ -5,17 +7,19 @@ hangman' :: String -> String -> Int -> IO ()
 hangman' s d n = do
     putStr "Enter a char: "
     i <- getChar
-    let sec = decodeSecret s (i:d)
+    let i' = toLower i
+    let sec = decodeSecret s (i':d)
     putStrLn ""
     putStrLn ("Secret " ++ sec)
     if not (checkIfDone sec) then
-        hangman' s (i:d) (if i `elem` d then n else n+1)
+        hangman' s (i':d) (if i' `elem` d then n else n+1)
     else 
         putStrLn ("Solved in " ++ show (n + 1) ++ " tries")
 
 
+
 decodeSecret :: String -> String -> String
-decodeSecret ss xs = [if s `elem` xs then s else '*' | s<-ss]
+decodeSecret ss xs = [if toLower s `elem` xs then s else '*' | s<-ss]
 
 checkIfDone :: String -> Bool
 checkIfDone s = '*' `notElem` s
