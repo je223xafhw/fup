00874a5f4115d263ce63136318b753ab0ee1fe3c
je223xafhw <stud105859@fh-wedel.de>
2023-06-12 17:18:45 +0200

    DOOOO

---
+++ code/04/08_do.hs
@@ -0,0 +1,12 @@
+getNonEmptyLine :: IO String
+getNonEmptyLine = do
+  input  <- getLine
+  result <- if null input 
+              then do putStrLn "Please enter a non-empty string."
+                      getNonEmptyLine
+              else return input
+  return result
+
+getNonEmptyLine2 :: IO String
+getNonEmptyLine2 = getLine >>= 
+    \input -> if null input then putStrLn "Please enter a non-empty string." >>= const getNonEmptyLine2 else return input
